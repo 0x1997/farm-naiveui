6d4a2680a9ad23bb4d23e7436ac8cb69235d6a82
wangzhe <0x1998@gmail.com>
2024-07-28 21:55:13 +0800

    first commit

---
+++ farm.config.ts
@@ -0,0 +1,25 @@
+import * as path from "path";
+import * as process from "process";
+
+import { defineConfig } from "@farmfe/core";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  compilation: {
+    presetEnv: {
+      options: {
+        targets: "Chrome >= 84",
+      },
+    },
+    resolve: {
+      alias: {
+        "@/": path.join(process.cwd(), "src"),
+      },
+    },
+    output: {
+      filename: 'static/[name].[hash].[ext]',
+      assetsFilename: 'static/[resourceName].[ext]'
+    },
+  },
+  vitePlugins: [vue()],
+});
